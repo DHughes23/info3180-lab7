4ae9e304df7caed0fe80eaa8b28ba9cc5c901c8a
Daniel Hughes <danielhughes23@hotmail.com>
2019-04-28 03:11:06 +0000

    Project we did

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,28 @@
 from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "change this to be a more random key"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project2:june1997@localhost/project2"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 csrf = CSRFProtect(app)
-app.config['SECRET_KEY'] = 'v\xf9\xf7\x11\x13\x18\xfaMYp\xed_\xe8\xc9w\x06\x8e\xf0f\xd2\xba\xfd\x8c\xda'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gkyzmbizacxrmw:75966ee0b33296949eb5fe93522abf4b35b570bb949101e3d6a163c6b4217f0f@ec2-50-17-246-114.compute-1.amazonaws.com:5432/dudgtvsqgan12'
+
+app.config['UPLOAD_FOLDER']='./app/static/uploads'
+
+
+db = SQLAlchemy(app)
+
+# Flask-Login login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+app.config.from_object(__name__)
+app.debug= True
 from app import views
-
-UPLOAD_FOLDER = "./app/static/uploads"
--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,30 @@
 from flask_wtf import FlaskForm
-from wtforms import FileField, TextAreaField
+from wtforms import StringField, PasswordField, TextAreaField
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms.validators import InputRequired
-from flask_wtf.file import FileAllowed
 
-class UploadForm(FlaskForm):
-    description = TextAreaField('Description', validators=[InputRequired()])
-    photo = FileField('Photo', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    firstname = StringField('First-name', validators=[InputRequired()])
+    lastname = StringField('Last-name', validators=[InputRequired()])
+    email = StringField('Email', validators=[InputRequired()])
+    location = StringField('Location', validators=[InputRequired()])
+    biography = TextAreaField('Biography',validators=[InputRequired()])
+    photo= FileField('Profile Photo',validators=[FileRequired(),FileAllowed(['jpg', 'png'], 'Images only!')])
+    
+class PostForm(FlaskForm):
+    user_id = StringField("", validators=[InputRequired()])
+    photo = FileField('Profile Picture', validators=[FileRequired(), FileAllowed(['jpg','png'],'Image only!')])
+    caption = TextAreaField('Caption', validators=[InputRequired()])  
+    
+    
+    
+    
+    
